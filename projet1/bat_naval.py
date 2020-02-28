d8b616123d33a29d72f8fca79217283cd7682752
RamiELB <rami.elbeblawy@gmail.com>
2020-02-28 15:59:39 +0100

    gustavoletryhard

--- projet1/bat_naval.py
+++ projet1/bat_naval.py
@@ -90,6 +90,9 @@ class grille():
         
         
     def trouver_coord(self):
+        """
+        Trouve les coordonnées ( tuples ) des bateaux. Rend une liste de Tuples 
+        """
         L=[]
         for i in range(10):
             for j in range(10):
@@ -112,15 +115,117 @@ class grille():
                 taille -= 1
                 i += 1
         
+######################## BATAILLE CLASS ##############################################################
+class Bataille ():
+    def __init__(self , grilleAlea):
+        self.grilleAlea=grilleAlea
     
+    
+    def joue(self,position): 
+        """Joue un coup dans les coordonnées du tuple position, si ça touche on rend true 
+            sinon false
+        """
+        i,j = position
+        if self.grilleAlea.tab[i][j] > 0 :
+            self.grilleAlea.tab[i][j] = 0 # On a coulé la case
+            return True
+        # rien touché
+        return False
+        
+
+    def victoire(self):
+        """
+        Rend true si tt les bateaux sont coulés, false s'il en reste des bateaux a detruire
+        """
+        ListeCoordBateau= self.grilleAlea.trouver_coord()
+        for (i,j) in ListeCoordBateau :
+            if self.grilleAlea(i,j) == 0:
+                continue
+            else:
+                return False
+        return True
+    #def reset (self):
+    
+    
+    
+    
+def coupsPossiblesGrille():
+    """FCT auxiliaire pr generer les coups possibles sous forme de liste:
+    """
+    coupsPossibles=[]
+    for i in range(10):
+            for j in range (10):
+                coupsPossibles.append( (i,j) )
+    return coupsPossibles
+
+############################# RANDOM_PLAYER CLASS ###################################################
+class Strategy():
+    """Class strategy qui represente la strategie pr gagner en trouvant les 17cases des bateaus
+    """
+    def __init__(self, total = 17):
+        self.miss = []
+        self.hits = []
+        self.total = total
+
+class RandomPlayer(Strategy):
+    """Strat aléatoire: On commence avec 1 grille alea et chaque tour on joue un coup random
+    """
+    def __init__(self, bataille , total = 17):
+        super(RandomPlayer,self).__init__(total)
+        self.bataille = bataille #bataille contient grille aleatoire et fcts ...
+        
+    def joueCoup(self):
+        """Joue un coup et retourne le nombre de coups ratés + coups reussis pr obtenir la victoire
+        """
+        uselessCPT=0
+        coupsPossibles=coupsPossiblesGrille() ## LISTE DES COUPS
+        #print("ici")
+        while (len(self.hits) < self.total):
+            #print("ici2")
+            coord_a_Jouer = random.choice(coupsPossibles) # selectionne un coup aleatoire de la liste
+            coupsPossibles.remove(coord_a_Jouer) # enleve ce coup de la liste
+            
+            if (self.bataille.joue(coord_a_Jouer)) == True :
+                self.hits.append(coord_a_Jouer)
+            else:
+                self.miss.append(coord_a_Jouer)
+                
+            #Affichage de l'état de la grille
+            if(uselessCPT%5==0):
+                self.bataille.grilleAlea.affiche()
+            uselessCPT+=1
+        print("Hits",len(self.hits))
+        print("Misses",len(self.miss))
+        
+        #Return la somme des miss et hits
+        return len(self.hits) + len(self.miss)
+
+############################ HEURESTIQUES_PLAYER CLASS###############################################
+
+class HeuresticPlayer(Strategy):
+    """ Strat avec regles(exploiter coup precedent ) :
+        1) Jouer random tant que on touche rien...
+        2) Qd on touche un bateau, on explore les cases connexes 
+    """
+    
+    
+    def __init__(self, bataille , total = 17):
+        super(HeuresticPlayer,self).__init__(total)
+        self.bataille = bataille
+
+    def joueCoup(self):
+        return len(self.hits)+len(self.miss)
+
+############################# BATEAU CLASS ##########################################################
+
 class bateau():
     def __init__(self, taille, type_bat):
         self.taille = taille
         self.type_bat = type_bat
-        
+
     def getTaille(self):
         return self.taille
-        
+#######################################################################################
 def genere_grille():
     g = grille()
     list_bat = []
@@ -293,4 +398,14 @@ def trouve_bonne_grille2():
         print(cpt)
     return cpt
 
-denombre_placer_3_bateaux()
+#denombre_placer_3_bateaux()
+###------------------------------#### Main:
+    
+
+grilleRandom= genere_grille()
+grilleRandom.affiche()
+battle = Bataille(grilleRandom)
+player = RandomPlayer(battle)
+compteur = player.joueCoup()
+
+print("total de coups joués par randomplayer :",compteur)
